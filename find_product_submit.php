1e8796211061ece75a835201c1d08abc51776a4f
Leon <Leon-student@users.noreply.github.com>
2016-09-01 15:58:04 +1000

    Add files via upload
    Search function implemented

---
+++ find_product_submit.php
@@ -0,0 +1,39 @@
+<?php
+	
+	session_start();							//start the session
+	
+	$errMsg = "";
+	$sku = htmlspecialchars(trim($_POST["find_product_sku"]));
+	$name = htmlspecialchars (trim ($_POST["find_product_name"]));
+	$type = htmlspecialchars (trim ($_POST["find_product_type"]));
+	
+
+	require_once( "php/settings.php" );		//connection info
+	$conn = @mysqli_connect( $host, $user, $pwd, $sql_db );
+
+	if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+	}
+
+	$query = "SELECT * FROM product 
+	WHERE sku = '$sku' or name = '$name' or type = '$type';";
+	$result = $conn->query($query);
+	if ($result->num_rows > 0) {
+    	while ($row = $result->fetch_assoc()) {
+        $success = "SKU: " . $row["sku"]. " <br>Name: " . $row["name"]. "<br> Type: " . $row["type"]. "<br>";
+    	}
+	} else {
+    	$errMsg = "No results found!";
+	}
+	mysqli_free_result ($result);
+	if ($errMsg != "") //check for errors
+	{
+		$_SESSION["find_product_result"] = "<p>$errMsg</p>";
+		header ("location:product_management.php");
+	}else
+	{
+		$_SESSION["find_product_result"] = "<p>$success</p>";
+		header ("location:product_management.php");
+	}
+	$conn->close();
+?>
---
+++ product_management.php
@@ -0,0 +1,93 @@
+<?php
+	session_start();							//start the session
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" 	content="Products Page of PHP-SRePS" />
+	<meta name="keywords" 		content="HTML5, tags" />
+	<meta name="author" 		content="Chris Hendrickson, Leon Vaisman, Claire Rochelmeyer"  />
+	<title>PHP-SRePS - Products</title>
+	<link href= "styles/style.css" rel="stylesheet"/>
+</head>
+
+<body>
+	<?php
+		$_SESSION[ "current" ] = "products";
+		require_once ("php/header.php");
+		require_once ("php/navigation.php");
+	?>
+	
+	<article>
+		<h2>Product Management</h2>
+		
+		<form method="post" id="add_product" action="add_product_submit.php" >
+			<fieldset>
+				<legend>Add Product</legend>
+				
+				<p><label for="add_product_sku">SKU</label>
+					<input type="text" name="add_product_sku" id="add_product_sku" maxlength="40" size="25" pattern="^[a-zA-Z0-9]+$" required="required" autofocus="autofocus" title="Must only be letters or numbers" />
+				</p>
+				<p><label for="add_product_name">Name</label>
+					<input type="text" name="add_product_name" id="add_product_name" maxlength="100" size="50" pattern="^[a-zA-Z0-9 -]+$" required="required" title="Can only contain A-Z, a-z, 0-9, and -" />
+				</p>
+				<p><label for="add_product_type">Type</label>
+					<input type="text" name="add_product_type" id="add_product_type" maxlength="100" size="50" pattern="^[a-zA-Z0-9 -]+$" required="required" title="Can only contain A-Z, a-z, 0-9, and -" />
+				</p>
+				<p><label for="add_product_price">Price Per Unit $</label>
+					<input type="number" name="add_product_price" id="add_product_price" step="0.01" min="0" max="99999999.99" required="required" />
+				</p>
+				<p><label for="add_product_quantity">Initial Quantity</label>
+					<input type="number" name="add_product_quantity" id="add_product_quantity" min="0" max="99999" required="required" />
+				</p>
+			</fieldset>
+		
+			<?php
+				if (isset ($_SESSION["add_product_result"]) && $_SESSION["add_product_result"] != "")
+				{
+					$message = $_SESSION["add_product_result"];
+					echo "<div id=errmsg>", $message, "</div>";
+					$_SESSION["add_product_result"] = "";
+				}
+			?>
+			
+			<input type="submit" value="Add Product" />
+		</form>
+		<br><br>
+		<form method="post" id="find_product" action="find_product_submit.php" >
+			<fieldset>
+				<legend>Find Product</legend>
+				
+				<p><label for="find_product_sku">SKU</label>
+					<input type="text" name="find_product_sku" id="find_product_sku" maxlength="40" size="25" pattern="^[a-zA-Z0-9]+$"  autofocus="autofocus" title="Must only be letters or numbers" />
+				</p>
+				<p><label for="find_product_name">Name</label>
+					<input type="text" name="find_product_name" id="find_product_name" maxlength="100" size="50" pattern="^[a-zA-Z0-9 -]+$"  title="Can only contain A-Z, a-z, 0-9, and -" />
+				</p>
+				<p><label for="find_product_type">Type</label>
+					<input type="text" name="find_product_type" id="find_product_type" maxlength="100" size="50" pattern="^[a-zA-Z0-9 -]+$"  title="Can only contain A-Z, a-z, 0-9, and -" />
+				</p>
+			</fieldset>
+			<?php
+				if (isset ($_SESSION["find_product_result"]) && $_SESSION["find_product_result"] != "")
+				{
+					$message = $_SESSION["find_product_result"];
+					echo "<div id=errmsg>", $message, "</div>";
+					$_SESSION["find_product_result"] = "";
+				}
+			?>
+			
+			<input type="submit" value="Find Product" />
+		</form>
+	</article>
+	
+	<?php
+		require_once ("php/footer.php");
+	?>
+</body>
+
+</html>
+
