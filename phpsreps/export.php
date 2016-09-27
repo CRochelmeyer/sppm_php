f06d737485a7b33df7274a178474783f82f14d2a
Chris Hendrickson <7381565@student.swin.edu.au>
2016-09-27 17:33:55 +1000

    Export Headers Fixed
    Unexpected headers in the csv have been removed. And the filename
    changed to _ for best practice. File close and sql close added.

--- phpsreps/export.php
+++ phpsreps/export.php
@@ -3,15 +3,19 @@ session_start();
 
 $from = $_SESSION['rdatefrom'];
 $to = $_SESSION['rdateto'];
+$host = "127.0.0.1";
+$user = "sdpm_user";
+$pwd = "dBw-Fdu-c6q-tXF";
+$sql_db = "sdpm_db";
 
-require_once("php/settings.php");
 $conn = @mysqli_connect( $host, $user, $pwd, $sql_db );
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$filename = "sr.".$from."to".$to.".csv";
-$fp = fopen('php://output', 'w');
+$filename = "sr_".$from."to".$to.".csv";
+$filename = str_replace("-","_",$filename);
 
 $sql="SELECT ps.product_id, ps.quantity_sold, s.sale_id, s.time_created, s.sale_amount, p.quantity, p.sku, p.name, p.type 
     FROM product_sale_item ps
@@ -19,19 +23,15 @@ $sql="SELECT ps.product_id, ps.quantity_sold, s.sale_id, s.time_created, s.sale_
     INNER JOIN product p ON p.product_id=ps.product_id
     WHERE s.time_created BETWEEN '$from' AND'$to';";
 
-$result = $conn->query($sql);
-  while ($row = mysqli_fetch_row($result)) {
-  $header[] = $row[0];
-}
-
-header('Content-type: application/csv');
+header('Content-type: text/csv');
 header('Content-Disposition: attachment; filename='.$filename);
-fputcsv($fp, $header);
 
-$num_column = count($header);   
+$fp = fopen('php://output', 'w');
+fputcsv($fp, array('Product ID', 'Quantity Sold', 'Sale ID', 'Sale Date', 'Sale Amount', 'Quantity', 'SKU', 'Name', 'Type')); 
 $result = $conn->query($sql);
 while($row = mysqli_fetch_row($result)) {
   fputcsv($fp, $row);
 }
-
+fclose($fp);
+$conn->close();
 ?>
